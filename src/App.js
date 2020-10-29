c559942b3c01e15dfa7bdb962f3ad6b377a05855
Michael Zlotkowski <michael.zlotkowski@gmail.com>
2020-10-29 08:42:38 -0700

    initial site load in thesh template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import P1 from "../src/assets/img/team/MZ.jpg";
+import P2 from "../src/assets/img/team/AK.jpg";
+import Header from "./components/Header";
+import Publications from "./components/Publications";
+import Contact from "./components/Contact";
 
 function App() {
+  const publicationLinks = [
+    {
+      image: P1,
+      title: "Mikey Zlotkowski",
+      caption: "Singer-Songwriter/Guitarist",
+      instagram: "https://www.instagram.com/thesh_mikey/",
+      contact: "https://www.tappy.tech/mikez",
+    },
+    {
+      image: P2,
+      title: "Andrew Keiser",
+      caption: "Producer/Drummer",
+      instagram: "https://www.instagram.com/andrewkeiser_/",
+      contact: "https://www.tappy.tech/AK15366",
+    },
+  ];
+
   return (
     <div className="App">
+      <Header />
+      <Publications publicationLinks={publicationLinks}></Publications>
+      <Contact />
     </div>
   );
 }
---
+++ src/components/Publications.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+export default ({ publicationLinks }) => {
+    return (
+        <section className="bg-gray page-section" id="portfolio">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <h2 className="section-heading text-uppercase">ORIGINAL MUSIC</h2>
+              <h3 className="section-subheading text-muted">Written and Performed By:</h3>
+            </div>
+          </div>
+          <div className="row">
+            {
+                publicationLinks && publicationLinks.map(({ image, title, caption , instagram, contact}, index) => 
+                    <div className="col-md-6 col-sm-6 portfolio-item">
+                        <a className="portfolio-link" data-toggle="modal" href={ contact }>
+                            <div className="portfolio-hover">
+                                <div className="portfolio-hover-content">
+                                </div>
+                            </div>
+                            <img className="img-fluid" src={ image } alt={ image } />
+                        </a>
+                        <div className="portfolio-caption">
+                            <h4>{ title }</h4>
+                            <p className="text-muted">{ caption }</p>
+                        </div>
+                        <ul className="list-inline social-buttons">
+                          <li className="list-inline-item">
+                            <a href= { instagram }> 
+                              <i className="fa fa-instagram"></i>
+                            </a>
+                          </li>
+                          <li className="list-inline-item">
+                            <a href= { contact }> 
+                              <i className="fa fa-id-badge text-white"></i>
+                            </a>
+                          </li>
+                        </ul>
+                    </div>
+                )
+            }
+          </div>
+        </div>
+      </section>
+    )
+}
